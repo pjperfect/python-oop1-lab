37990d2046a4588984f35335e6ee2991f20e07db
Philip Olembo <pjole.kenya@gmail.com>
2026-03-09 14:26:40 +0300

    Lab: Object Oriented Programming- Bookstore

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        self._page_count += 1
+
+    def get_page(self):
+        return self._page_count
     
+    def set_page(self, value):
+        if type(int) is int and value >= 0:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page, set_page)
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -2,3 +2,22 @@
 
 class Coffee:
     pass
+    def __init__(self, size, price):
+        self._size = size
+        self.price = price
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, value):
+        value = value.lower()
+        if value in ["small", "medium", "large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    size = property(get_size, set_size)
